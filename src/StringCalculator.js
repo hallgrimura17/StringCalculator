65cc2abfb1f589e88e01ce04605e48401581d63d
Hallgrimur Andresson <hallgrimura17@ru.is>
2018-10-11 16:54:25 +0000

    Implemented add for a single number as input

--- src/StringCalculator.js
+++ src/StringCalculator.js
@@ -1,6 +1,13 @@
 function add(numbers)
 {
-    return 0;
+    if(numbers == "")
+    {
+        return 0;
+    }
+    else
+    {
+        return parseInt(numbers);
+    }
 }
 
 module.exports = add;
--- src/StringCalculator.test.js
+++ src/StringCalculator.test.js
@@ -3,3 +3,6 @@ const add = require("./StringCalculator");
 it("should return zero on an empty string", () => {
     expect(add("")).toBe(0);
 });
+it("should return the number given", () => {
+    expect(add("1")).toBe(1);
+});
